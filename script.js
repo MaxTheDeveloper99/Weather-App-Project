b3a60cc5295c50e16d725a407b06f1cb1ec629b3
Maxwell Ogbekhilu <maxogbekhilu@gmail.com>
2024-12-28 21:22:59 +0100

    API Key

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "e89e4ea21db1bd6a7d9d520fcde559c0"; // API key for OpenWeatherMap API
+const API_KEY = "API key for OpenWeatherMap API"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
